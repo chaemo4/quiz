6cd3ef71194842917280fb5a9b567a59fb0330c7
chaemo4 <kcanalim@gmail.com>
2024-07-02 17:21:10 +0900

    quiz
    quiz

---
+++ quiz.py
@@ -0,0 +1,16 @@
+#회원가입 1
+name = input("이름을 입력하세요.")
+age = input("나이를 입력하세요.")
+email = input("이메일 주소를 입력하세요.")
+phone = input("연락처를 입력하세요.")
+dic = {name: [age, email, phone]}
+
+print(dic)
+#회원가입 2
+name1 = input("이름을 입력하세요.")
+age1 = input("나이를 입력하세요.")
+email1 = input("이메일 주소를 입력하세요.")
+phone1 = input("연락처를 입력하세요.")
+dic = {name: [age1, email1, phone1]}
+
+print(dic1)
